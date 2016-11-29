fb704b48280271ba2e448cfa3cd85373b14cb8b5
Cristof17 <cristof_rotsching@yahoo.com>
2016-11-29 21:54:20 +0200

    First commit

---
+++ src/Generator.java
@@ -0,0 +1,11 @@
+
+public class Generator extends Thread{
+
+	@Override
+	public void run() {
+		
+	}
+	
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		if (args.length < 6){
+			System.out.println("Too few argumens");
+			return;
+		}
+	}
+}
